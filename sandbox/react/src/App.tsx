63af4941b33aac9250135449f23c7f7f98e6bf16
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2024-02-22 15:34:04 +0100

    chore: add react sandbox

---
+++ sandbox/react/src/App.tsx
@@ -0,0 +1,17 @@
+import { css } from '../styled-system/css'
+
+function App() {
+  return (
+    <>
+      <div
+        className={css({
+          color: 'red.100',
+          backgroundColor: '#fff',
+          fontSize: '123px',
+        })}
+      ></div>
+    </>
+  )
+}
+
+export default App
