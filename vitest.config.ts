ef7ed349bcedc192f2a0739de9d40a5e6d99cab7
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2024-02-23 19:21:42 +0100

    fix: ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vitest/config'
 
+const resolve = (val: string) => new URL(val, import.meta.url).pathname
+
 export default defineConfig({
+  root: process.cwd(),
   test: {
     hideSkippedTests: true,
   },
