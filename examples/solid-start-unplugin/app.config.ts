235349be34c4d07a549c58053d591c71cb5d1717
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2024-04-10 10:58:43 +0200

    chore(examples): add @pandacss/dev + @pandabox/unplugin

--- examples/solid-start-unplugin/app.config.ts
+++ examples/solid-start-unplugin/app.config.ts
@@ -1,3 +1,18 @@
-import { defineConfig } from "@solidjs/start/config";
+import { defineConfig } from '@solidjs/start/config'
+import pandabox from '@pandabox/unplugin'
 
-export default defineConfig({});
+const isDev = process.env.NODE_ENV === 'development'
+
+export default defineConfig({
+  //   middleware: './src/middleware.ts',
+  //   server: {
+  //     preset: 'cloudflare-module',
+  //   },
+  vite: {
+    plugins: [
+      pandabox.vite({
+        optimizeCss: !isDev,
+      }),
+    ],
+  },
+})
